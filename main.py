d812aaa314ddfab262de6744a9bdff7f94b8c70e
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs.uni-erlangen.de>
2016-09-21 17:53:14 +0200

    scatter plot

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from bokeh.layouts import row, column
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Select
 from bokeh.io import curdoc
+from bokeh.models import Jitter
+from bokeh.models import HoverTool
 
 df = pd.read_pickle("out.pandas")
 
@@ -25,13 +27,18 @@ df = pd.melt(
 df[['track_id']] = df[['track_id']].apply(pd.to_numeric)
 df['subset'] = np.where(df['track_id'] >= 51, 'Dev', 'Test')
 
+estimate_names = sorted(df.estimate_name.unique().tolist())
+track_ids = map(str, df.track_id.unique().tolist())
+
 subset = 'Dev'
 measure = 'SDR'
 target = 'vocals'
+track_id = '1'
 
 subset_select = Select(value=subset, title='Subset', options=subsets)
 measure_select = Select(value=measure, title='Measure', options=measures)
 target_select = Select(value=target, title='Target', options=targets)
+track_id_select = Select(value=track_id, title='Track ID', options=track_ids)
 
 df = df.dropna()
 df_measure = df[
@@ -41,16 +48,20 @@ df_measure = df[
 ]
 
 source = ColumnDataSource(
-    data=dict(x=[], y=[], metric=[], target=[], subset=[])
+    data=dict(x=[], y=[], metric=[], target=[], subset=[], track_id=[])
 )
 
+hover = HoverTool(tooltips=[
+    ("Track ID", "@track_id"),
+    ("Score", "@y"),
+])
+
 p = figure(
-    plot_height=600,
-    plot_width=700,
+    plot_height=400,
+    plot_width=1024,
     title="Fu",
-    tools="pan,wheel_zoom,box_select,lasso_select,reset",
-    y_range=(-10, 10),
-    x_range=(0, 10)
+    tools=[hover, "pan,wheel_zoom,box_select,lasso_select,reset"],
+    x_range=estimate_names
 )
 p.circle(
     x="x",
@@ -58,9 +69,11 @@ p.circle(
     source=source,
     size=7,
     color="orange",
-    line_color="#3A5785",
-    fill_alpha=1
+    fill_alpha=.5,
+    line_width=0.1,
+    line_color="orange"
 )
+
 
 controls = [subset_select, measure_select, target_select]
 
@@ -74,17 +87,22 @@ def update():
         (df.target_name == target) &
         (df.subset == subset)
     ]
-    df_measure.fillna(0, inplace=True)
+    d = ColumnDataSource(data=df_measure)
+
+    # df_measure.fillna(0, inplace=True)
     p.xaxis.axis_label = "Estimate"
     p.yaxis.axis_label = measure_select.value
+    p.title.text = subset
+    p.y_range.start = min(d.data['score']) - 3
+    p.y_range.end = max(d.data['score']) + 3
 
-    d = ColumnDataSource(data=df_measure)
     source.data = dict(
         x=d.data['estimate_name'],
         y=d.data['score'],
         metric=d.data["metric"],
         target=d.data["target_name"],
         subset=d.data["subset"],
+        track_id=d.data["track_id"]
     )
 
 update()
