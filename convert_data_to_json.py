dc52125569a2b5fc3bda340f01dedfb9028a26c2
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs.uni-erlangen.de>
2016-12-04 14:07:37 +0100

    add subset information

--- convert_data_to_json.py
+++ convert_data_to_json.py
@@ -39,14 +39,17 @@ if __name__ == '__main__':
     headers['targets'] = list(
         df['target_name'].astype('category').cat.categories
     )
+    headers['subsets'] = ['Dev', 'Test']
+
     with open("headers.json", 'w') as f:
         json.dump(headers, f)
 
     # fetch track_id
-    df[['track_id']] = df[['track_id']].astype(np.int16)
-    df['target_name'] = df['target_name'].astype('category').cat.codes
-    df['estimate_name'] = df['estimate_name'].astype('category').cat.codes
-    df['metric'] = df['metric'].astype('category').cat.codes
+    df['track_id'] = df['track_id'].astype(np.int16)
+    df['subset'] = np.where(df['track_id'] >= 51, 0, 1)
+    df['target_id'] = df['target_name'].astype('category').cat.codes
+    df['estimate_id'] = df['estimate_name'].astype('category').cat.codes
+    df['metric_id'] = df['metric'].astype('category').cat.codes
 
     df.to_csv(
         path_or_buf="out.csv",
